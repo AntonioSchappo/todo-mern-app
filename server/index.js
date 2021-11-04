dce458196d98fe08e623687f5879539ad6e3cab5
Antonio Schappo <antonioschappo@gmail.com>
2021-11-03 21:33:25 -0300

    setup express on index file

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Live on port ${port}`));
